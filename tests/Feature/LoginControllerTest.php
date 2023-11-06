457dfda794a854c41970194527b5380bac3b85b2
Mauricio Pereira <mauricio.pereira.webart@gmail.com>
2023-11-06 20:46:06 -0300

    Feat: Criando os testes automatizados para o login

---
+++ tests/Feature/LoginControllerTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginControllerTest extends TestCase
+{
+    /** @test */
+    public function test_login_user(): void
+    {
+        // Faz o login
+        $response = $this->reuseLogin();
+
+        $response->assertStatus(200)
+        ->assertJsonStructure([
+            "user",
+            "token",
+         ]);
+    }
+
+    /** @test */
+    public function test_logout_user() : void{
+        $user = $this->reuseLogin();
+        
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $user['token'],
+        ])->json('POST', 'api/logout');
+
+        $response->assertStatus(200)
+        ->assertExactJson(['message' => 'sucesso']);
+    }
+
+    /** @test */
+    public function test_me_user() : void{
+        $user = $this->reuseLogin();
+        
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $user['token'],
+        ])->json('GET', 'api/me');
+
+        $response->assertStatus(200)
+        ->assertJsonStructure([
+            "user"
+        ]);
+    }
+
+    /**
+     * Reutilização do login
+     */
+    public function reuseLogin() : object{
+        // Crie um usuário no banco de dados
+        $user = User::factory()->create();
+
+        // Seta o device
+        $user->device_name = 'testeControllerLogin';
+
+        // Faz o login
+        return  $this->json('POST', '/api/login', [
+            'email' => $user->email,
+            'password' => 'password',
+            'device_name' => $user->device_name
+        ]);
+    }
+}
